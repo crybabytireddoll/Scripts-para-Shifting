08143d3393f4acc268ef129e8328f7cff01343b4
crybabytireddoll <crybaby.tireddoll@gmail.com>
2024-09-08 18:44:16 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "O que é Shifting?",
+        descricao : "O Shifting, popularmente conhecido como mudança de realidade. A prática, que envolve técnicas de visualização, meditação e indução de estados alterados de consciência, busca transportar o indivíduo para realidades alternativas, onde seus desejos seriam realizados.",
+        link : "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8556810/",
+        tags: "shifting dr DR realidade desejada"
+    },
+    {
+        titulo : "Script Base",
+        descricao : "Um script base é uma ferramenta versátil que pode ser utilizada para facilitar a criação dos mais diversos roteiros e garantir que eles sejam claros, concisos e eficazes. Ao usar um script base, você está investindo em um processo mais organizado e produtivo.",
+        link : "https://docs.google.com/document/d/16l1KfY6EyoStX5FPsy6ZJh2nZaPZjI8L2TE0_GoDm70/edit?usp=sharing",
+        tags: "simples roteiro completo"
+    },
+    {
+        titulo : "Script Hogwarts",
+        descricao : "Hogwarts, a Escola de Magia e Bruxaria. Fundada por volta do ano 993 d.C., por quatro poderosos bruxos, a escola tem uma longa e rica história repleta de mistérios, aventuras e magia.",
+        link : "https://docs.google.com/document/d/1c4M5s4OwxILTlBGo439dhrzbhbDe1iVrfVbbQ7TJnO8/edit?usp=sharing",
+        tags: "hogwarts escola magia harry potter Harry Potter"
+    }
+];
+
